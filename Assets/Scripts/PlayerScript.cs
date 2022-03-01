b019b233a65cc97d3478666ad9ad97656d690a0b
Maria <mjm1175@nyu.edu>
2022-03-01 15:27:35 -0500

    added color changing and city level

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,6 +15,13 @@ public class PlayerScript : MonoBehaviour
 
     private float size = 1.0f;
 
+    private int color_id_sz = 5;
+    private int curr_color_id = 0;     // corresponds to 'r'
+    private char curr_color_color = 'r';        // used for pickup
+    private char[] color_id = {'r', 'g', 'b', 'y', 'p'};
+    //                                             'r'                                          'g'                                 'b'                                     'y'                                         'p'
+    private Color[] color_colors = {new Color(0.706f, 0.059f, 0.059f, 1.0f), new Color(0.416f, 0.659f, 0.204f, 1.0f), new Color(0.145f, 0.498f, 0.737f, 1.0f), new Color(0.867f, 0.745f, 0.0f, 1.0f), new Color(0.659f, 0.4f, 0.953f, 1.0f)};
+
     // Start is called before the first frame update
     // Create event
     void Start()
@@ -28,6 +35,18 @@ public class PlayerScript : MonoBehaviour
     // Step event
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C)){
+            curr_color_id++;
+            if (curr_color_id >= color_id_sz) curr_color_id = 0;
+            curr_color_color = color_id[curr_color_id];
+            GetComponentInChildren<Renderer>().material.color = color_colors[curr_color_id];
+        }
+
+        if (size <= 0.01) {
+            SceneManager.LoadScene("Game_Over");
+        }
+
+
         size = transform.localScale.x;
         Debug.Log("size: " + size);
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
@@ -55,21 +74,20 @@ public class PlayerScript : MonoBehaviour
             animator.SetBool("moving", false);
             transform.Rotate(0, input.x * nma.angularSpeed * Time.deltaTime, 0);
         }
-
-        if (size <= 0.01) {
-            SceneManager.LoadScene("Game_Over");
-        }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Pickup") && other.transform.localScale.magnitude <= transform.localScale.magnitude){
-            // increasing size by half of absorbed object's size
-            size += other.transform.localScale.magnitude / transform.localScale.magnitude;
-            Vector3 newSize = new Vector3(size, size, size);
-            transform.localScale = newSize;
+            // only if same color
+            if (other.GetComponent<PickupScript>().color == curr_color_color){
+                // increasing size by half of absorbed object's size
+                size += other.transform.localScale.magnitude / transform.localScale.magnitude;
+                Vector3 newSize = new Vector3(size, size, size);
+                transform.localScale = newSize;
 
-            // destroying the object we collected
-            Destroy(other.gameObject);
+                // destroying the object we collected
+                Destroy(other.gameObject);                
+            }
         }
     }
 }
