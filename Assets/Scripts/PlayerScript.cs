658e5baca062ccd7fc5639b97e0d87e89861fe31
Maria <mjm1175@nyu.edu>
2022-03-03 00:07:38 -0500

    playable forest level, added enemy eating

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,8 +25,9 @@ public class PlayerScript : MonoBehaviour
     private char curr_color_color = 'r';        // used for pickup
     private char[] color_id = {'r', 'g', 'b', 'y', 'p'};
     //                                             'r'                                          'g'                                 'b'                                     'y'                                         'p'
-    private Color[] color_colors = {new Color(0.706f, 0.059f, 0.059f, 1.0f), new Color(0.416f, 0.659f, 0.204f, 1.0f), new Color(0.145f, 0.498f, 0.737f, 1.0f), new Color(0.867f, 0.745f, 0.0f, 1.0f), new Color(0.659f, 0.4f, 0.953f, 1.0f)};
+    private Color[] color_colors = {new Color(0.706f, 0.059f, 0.059f, 1.0f), new Color(0.416f, 0.659f, 0.204f, 1.0f), new Color(0.216f, 0.384f, 0.945f, 1.0f), new Color(0.867f, 0.745f, 0.0f, 1.0f), new Color(0.659f, 0.4f, 0.953f, 1.0f)};
 
+    private Coroutine thisCollisionCrt;
 
     // Start is called before the first frame update
     // Create event
@@ -45,6 +46,7 @@ public class PlayerScript : MonoBehaviour
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         size = transform.localScale.x;
         sizeBar.SetSize(size);
+        nma.speed = size * size + 5;
         Debug.Log("size: " + size);
 
         // color change
@@ -89,7 +91,10 @@ public class PlayerScript : MonoBehaviour
 
         //death
         if (transform.localScale.x < 0.2){
-            if (!dying) StartCoroutine(DeathScene());
+            if (!dying){
+                StopAllCoroutines();
+                StartCoroutine(DeathScene());
+            } 
             dying = true;
             return;
         }
@@ -100,7 +105,7 @@ public class PlayerScript : MonoBehaviour
             GameObject newBullet = Instantiate(bulletPrefab, spawnPoint.position, transform.rotation);
             newBullet.GetComponent<Renderer>().material.color = color_colors[curr_color_id];
             newBullet.transform.localScale *= transform.localScale.x;
-            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 200);
+            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 200 *size);
         }
 
         //shrinking
@@ -155,11 +160,35 @@ public class PlayerScript : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy")){
             animator.SetBool("moving", false);
             animator.SetBool("colliding", true);
+
+            // if you're bigger and blue (enemy color)
+            if (other.transform.localScale.magnitude <= transform.localScale.magnitude && curr_color_color == 'b'){
+                // increasing size by half of absorbed object's size
+                size += other.transform.localScale.magnitude / transform.localScale.magnitude;
+                Vector3 newSize = new Vector3(size, size, size);
+                transform.localScale = newSize;
+
+                // destroying the object we collected
+                Destroy(other.gameObject);        
+            } else {
+                //maybe we can add some bullet like objects spawning to indicate that losing health
+                thisCollisionCrt = StartCoroutine(GetHurt(other.transform.localScale.magnitude, size));                
+            }
+        }
+    }
+
+    IEnumerator GetHurt(float otherMag, float this_size){
+        while (true){
+            this_size -= 0.1f;
+            Vector3 newSize = new Vector3(this_size, this_size, this_size);
+            transform.localScale = newSize;
+            yield return new WaitForSeconds(3f);
         }
     }
 
     private void OnCollisionExit(Collision other) {
         if (other.gameObject.CompareTag("Enemy")){
+            StopCoroutine(thisCollisionCrt);
             animator.SetBool("colliding", false);
         }
     }
